3c1b2af32c74c4f47adfd588f6e6aadb3b203d50
Tony <tomcatbuzz@yahoo.com>
2021-01-20 16:57:09 -0500

    small change

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-// import { GLTFLoader } from 'js/GLTFLoader.js';
+// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 import fragment from './shader/fragment.glsl';
 import vertex from './shader/vertex.glsl';
